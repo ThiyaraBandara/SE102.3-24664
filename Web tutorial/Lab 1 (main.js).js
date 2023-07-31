26d31b55912369b0f102872e5f6e2f408a245b3a
ThiyaraBandara <138982056+ThiyaraBandara@users.noreply.github.com>
2023-07-31 14:59:10 +0530

    Add files via upload

---
+++ Web tutorial/Lab 1 (main.js).js
@@ -0,0 +1,11 @@
+document.write("External Javascript is fun.<br>")
+
+
+no1 = parseInt(prompt("Enter the number 1"));
+no2 = parseInt(prompt("Enter the number 2"));
+
+document.write("Addition: " + (no1 + no2) + "<br>");
+document.write("Substraction: " + (no1 - no2) + "<br>");
+document.write("Multiplication: " + (no1 * no2) + "<br>");
+document.write("Division: " + (no1 / no2) + "<br>");
+
